94cd228c80589a82f9a74157f78f34ba7057bfb4
Léonard Alarie <lelecopter@gmail.com>
2023-01-27 13:48:53 -0500

    Initial push

---
+++ Assets/Editor/ShowTeam.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class ShowTeam
+{
+    static ShowTeam()
+    {
+        var team = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Equipe.txt");
+        if (team == null)
+        {
+            Debug.LogWarning("Réimportation d'assets, SVP redémarrer l'éditeur");
+            return;
+        }
+        if (team.text == "Membres de l'équipe")
+        {
+            EditorUtility.DisplayDialog("Identification", "N'oubliez pas d'indiquer le nom de vos coéquipiers dans le fichier \"Assets/Equipe.txt\"!!", "Compris!");
+        }
+
+        Debug.Log(team.text);
+    }
+}
